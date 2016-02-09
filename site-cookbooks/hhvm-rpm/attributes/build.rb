46c898096db2629ea6cd97834ab1dfaa115419e5
sawanoboly <sawanoboriyu@higanworks.com>
2016-02-09 23:06:58 +0900

    copy oss-remo-mode as hhvm-repo-mode

--- site-cookbooks/hhvm-rpm/attributes/build.rb
+++ site-cookbooks/hhvm-rpm/attributes/build.rb
@@ -2,7 +2,7 @@ default[:hhvm_rpm][:build] = {
   name: "hhvm",
   src: "https://github.com/facebook/hhvm.git",
   version: '3.9.1',
-  release: 3,
+  release: 4,
   cmake_opts: %w[
     -DENABLE_ZEND_COMPAT=ON
     -DMYSQL_UNIX_SOCK_ADDR=/var/lib/mysql/mysql.sock
