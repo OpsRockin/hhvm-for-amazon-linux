c512f2b93c2cab165a77ab8b6362667e067e5579
sawanoboly <sawanoboriyu@higanworks.com>
2016-06-04 13:29:35 +0900

    3.12.2

--- site-cookbooks/hhvm-rpm/attributes/build.rb
+++ site-cookbooks/hhvm-rpm/attributes/build.rb
@@ -1,8 +1,8 @@
 default[:hhvm_rpm][:build] = {
   name: "hhvm",
   src: "https://github.com/facebook/hhvm.git",
-  version: '3.12.1',
-  release: 2,
+  version: '3.12.2',
+  release: 1,
   cmake_opts: %w[
     -DENABLE_ZEND_COMPAT=ON
     -DMYSQL_UNIX_SOCK_ADDR=/var/lib/mysql/mysql.sock
